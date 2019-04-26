e07deb83bcd02b5f1a55a922c34457555046c443
willow <shaneweth77@gmail.com>
2019-04-26 17:22:23 -0400

    list is populating with user input

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,30 @@
 const addItems = document.querySelector('.add-items');
-const itmesList = document.querySelector('.plates');
+const itemsList = document.querySelector('.plates');
 const items = [];
+
+function addItem(e) {
+    e.preventDefault();
+    // "this" is <form>
+    const text = (this.querySelector('[name=item]')).value;
+    const item = {
+        text,
+        done: false
+    };
+    // reset method clears the text from the input after submit
+    items.push(item);
+    populateList(items, itemsList);
+    this.reset();
+}
+
+function populateList(plates = [], platesList) {
+        platesList.innerHTML = plates.map((plate, i) => {
+            return `
+            <li>
+                <input type="checkbox" data-index=${i} id="item${i}" ${plate.done ? 'checked' : ''} />
+                <label for="">${plate.text}</label>
+            </li>`
+            ;
+        }).join('');
+}
+
+addItems.addEventListener('submit', addItem);
